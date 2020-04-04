553333aa87bfa938db20c46d004e886624ad6504
Jacob Dahl <dahl.jakejacob@gmail.com>
2020-04-03 21:24:44 -0600

    adde check for pack voltage in startup code

--- boards/ark/bms/src/ark_startup.cpp
+++ boards/ark/bms/src/ark_startup.cpp
@@ -8,6 +8,11 @@
 #define BATT_SMBUS_VOLTAGE                              0x09
 #define BATT_SMBUS_CURRENT                              0x0A
 #define BATT_SMBUS_RELATIVE_SOC							0x0D
+#define MAC_BA_DATA_BUFFER_SIZE                         32 // MAC buffer size
+#define BQ40Z80_MAC_BA_DASTATUS1                        0x0071 ///< returns the cell voltages, pack voltage, bat voltage, cell currents, cell powers, power, and average power
+#define BQ40Z80_DASTATUS1_PACK_VOLTAGE_MSB              11
+#define BQ40Z80_DASTATUS1_PACK_VOLTAGE_LSB              10
+#define BATT_SMBUS_MANUFACTURER_BLOCK_ACCESS            0x44
 
 static SMBus* _interface;
 
@@ -24,8 +29,8 @@ __END_DECLS
 
 void update_battery_data(void);
 void update_led_status(void);
-
-
+bool check_pack_voltage(void);
+int manufacturer_read(const uint16_t cmd_code, void *data, const unsigned length);
 
 void ark_display_bq_startup_init(void)
 {
@@ -55,6 +60,11 @@ void ark_check_button_and_update_display()
 
 	auto start_time = hrt_absolute_time();
 	auto time_now = start_time;
+
+	// If we have voltage on PACK+ then boot immediately
+	if (check_pack_voltage()) {
+		return;
+	}
 
 	// Button should be held for 2.5 out of 3 seconds
 	while (time_now < (start_time + BUTTON_SHUTDOWN_DURATION_US)) {
@@ -92,16 +102,15 @@ void ark_check_button_and_update_display()
 		return;
 
 	} else {
-		printf("Button not held, powering off\n");
-
-		ssd1306::displayOff();
-		stm32_gpiowrite(GPIO_LED_5, true);
-		stm32_gpiowrite(GPIO_LED_4, true);
-		stm32_gpiowrite(GPIO_LED_3, true);
-		stm32_gpiowrite(GPIO_LED_2, true);
-		stm32_gpiowrite(GPIO_LED_1, true);
-		stm32_gpiowrite(GPIO_PWR_EN, false);
-		while(1){;};
+			printf("Button not held, powering off\n");
+			ssd1306::displayOff();
+			stm32_gpiowrite(GPIO_LED_5, true);
+			stm32_gpiowrite(GPIO_LED_4, true);
+			stm32_gpiowrite(GPIO_LED_3, true);
+			stm32_gpiowrite(GPIO_LED_2, true);
+			stm32_gpiowrite(GPIO_LED_1, true);
+			stm32_gpiowrite(GPIO_PWR_EN, false);
+			while(1){;};
 	}
 }
 
@@ -174,4 +183,51 @@ void update_led_status(void)
 		stm32_gpiowrite(GPIO_LED_2, true);
 		stm32_gpiowrite(GPIO_LED_1, true);
 	}
+}
+
+bool check_pack_voltage(void)
+{
+	// Check PACK to determine if FETs are open or if we are connected to a charger
+	uint8_t status[MAC_BA_DATA_BUFFER_SIZE];
+	int result = manufacturer_read(BQ40Z80_MAC_BA_DASTATUS1, &status, MAC_BA_DATA_BUFFER_SIZE);
+
+	if (result != PX4_OK) {
+		printf("reading DAStatus1 failed\n");
+		return true;
+	}
+
+	float pack_voltage = (float)(((uint16_t)status[BQ40Z80_DASTATUS1_PACK_VOLTAGE_MSB] << 8) | status[BQ40Z80_DASTATUS1_PACK_VOLTAGE_LSB]) / 1000.0f;
+
+	printf("pack voltage: %f\n", (double)pack_voltage);
+
+	if (pack_voltage > 3.5f) {
+		return true;
+	}
+
+	return false;
+}
+
+int manufacturer_read(const uint16_t cmd_code, void *data, const unsigned length)
+{
+	uint8_t code = BATT_SMBUS_MANUFACTURER_BLOCK_ACCESS;
+
+	uint8_t address[2] = {};
+	address[0] = ((uint8_t *)&cmd_code)[0];
+	address[1] = ((uint8_t *)&cmd_code)[1];
+
+	int result = _interface->block_write(code, address, 2, false);
+
+	if (result != PX4_OK) {
+		return result;
+	}
+
+	// Intermediary buffer to ensure we read the entire returned block
+	uint8_t buf[34] = {};
+
+	result = _interface->block_read(code, buf, length + 1, false);
+
+	// Drop the 2 leading bytes of address info
+	memcpy(data, &buf[2], length);
+
+	return result;
 }
